55aed1c4f96cf8478646f83e4d1b16c7072e3ff1
Kyle Reczek <savethejets@hotmail.com>
2013-08-26 01:00:04 -0600

    nicerized all levels

--- src/myApp.js
+++ src/myApp.js
@@ -25,8 +25,8 @@
  ****************************************************************************/
 
 var Keys = {},
-    levelIndex = 4,
-    isDebug = true;
+    levelIndex = 1,
+    isDebug = false;
 
 var keyColors = [
     new cc.Color3B(0,255,8), new cc.Color3B(0,115,255),
